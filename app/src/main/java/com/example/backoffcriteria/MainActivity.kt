f17ef88298ac058d1fc6e3c4dc693639d8faba51
Administrator <godxuche@gmail.com>
2021-10-26 15:26:14 +0100

    created a Worker class, WorkerFail and set onClickListener on the btnWorkerFail...

--- app/src/main/java/com/example/backoffcriteria/MainActivity.kt
+++ app/src/main/java/com/example/backoffcriteria/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.backoffcriteria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.backoffcriteria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val workManager = WorkManager.getInstance(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,7 +21,8 @@ class MainActivity : AppCompatActivity() {
         val btnWorkerRetry = binding.btnWorkerRetry
 
         btnWorkerFail.setOnClickListener {
-
+            val workRequest = OneTimeWorkRequestBuilder<WorkerFail>().build()
+            workManager.enqueue(workRequest)
         }
     }
 }
