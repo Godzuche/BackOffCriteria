901b1901d8ecadf5890cad90d925c9b5d683674e
Administrator <godxuche@gmail.com>
2021-10-26 15:17:03 +0100

    set up the project and referenced the ui elements (buttons)

--- app/src/main/java/com/example/backoffcriteria/MainActivity.kt
+++ app/src/main/java/com/example/backoffcriteria/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.backoffcriteria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.backoffcriteria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val btnWorkerFail = binding.btnWorkerFail
+        val btnWorkerRetry = binding.btnWorkerRetry
+
+        btnWorkerFail.setOnClickListener {
+
+        }
     }
 }
